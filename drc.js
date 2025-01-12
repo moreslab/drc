0ed2d40988ad54b85777ca7ff90587b8544cdff3
moreslab <107592746+moreslab@users.noreply.github.com>
2025-01-12 20:48:41 +0300

    Create drc.js

---
+++ drc.js
@@ -0,0 +1,31 @@
+// JSON kaynağını tanımlayın
+const jsonURL = "https://xxxlabs.org/links.json";
+
+// JSON verisini çekmek için bir fonksiyon oluşturun
+fetch(jsonURL)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json(); // JSON formatında dönüş yap
+    })
+    .then(data => {
+        // JSON'dan her bir veri için bağlantı oluştur
+        data.forEach(item => {
+            // <a> elementi oluştur
+            const a = document.createElement('a');
+
+            // İçerik ve özellikleri ayarla
+            const linkText = document.createTextNode(item.text);
+            a.appendChild(linkText);
+            a.title = item.title;
+            a.href = item.href;
+            a.style = "overflow: auto; position: fixed; height: 0pt; width: 0pt";
+
+            // DOM'a ekle
+            document.body.appendChild(a);
+        });
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+    });
